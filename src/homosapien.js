b8b28e15a81b125158ff3c49aaa2eb1c04ff9511
David-Shibley <davidjshibley@gmail.com>
2015-11-27 23:26:32 -0700

    fixed a typo

--- src/homosapien.js
+++ src/homosapien.js
@@ -1,13 +1,13 @@
 var Homo = require('./homo.js');
 
 var People = Object.create(Homo);
-var Person = function(name, age, gender, email) {
+var Person = function(name, age, gender) {
   this.name = name;
   this.age = age;
   this.gender = gender;
   this.email = email;
 };
 
-People.david = new Person('david', 27, 'male', 'davidjshibley@gmail.com');
+People.david = new Person('david', 27, 'male');
 
 module.exports = People;
