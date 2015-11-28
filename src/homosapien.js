338185a4e631d04429fddc78443d1679bbe3ca89
David-Shibley <davidjshibley@gmail.com>
2015-11-27 23:29:27 -0700

    fixed a typo

--- src/homosapien.js
+++ src/homosapien.js
@@ -5,7 +5,6 @@ var Person = function(name, age, gender) {
   this.name = name;
   this.age = age;
   this.gender = gender;
-  this.email = email;
 };
 
 People.david = new Person('david', 27, 'male');
